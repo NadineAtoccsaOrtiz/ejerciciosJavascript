0d1292e9ebfcb7cab9baedfd4f977c6f087cd2bc
NadineAtoccsaOrtiz <nadineatoccsaortiz@gmail.com>
2024-10-30 13:17:07 -0500

    4.imprimirArreglo

---
+++ 4.imprimirArreglo/ejercicio.js
@@ -0,0 +1,14 @@
+// escribe tu respuesta acá
+function imprimirArreglo(a, b, c, d) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+  }
+  
+  // Código de prueba
+  imprimirArreglo(1, "Hola", 2, "Mundo");
+  // 1 
+  // Hola 
+  // 2 
+  // Mundo
