63b0b5d814097a855fd1a73b7879eb83fbda8392
NadineAtoccsaOrtiz <nadineatoccsaortiz@gmail.com>
2024-10-30 13:23:24 -0500

    5.numeroLikes

--- 5.numeroLikes/ejercicio.js
+++ 5.numeroLikes/ejercicio.js
@@ -1,20 +1,13 @@
 // escribe tu respuesta acá
-function likes(numero) {
-    if (numero < 1000) {
-        return (numero);
-
-    } else if (numero < 1000000) {
-        let mil = numero / 1000;
-        if (mil % 1 >= 0.5) mil++; 
-        return (mil | 0) + "K"; 
-
+function likes(num) {
+    if (num < 1000) {
+      return String(num);
+    } else if (num >= 1000 && num < 1000000) {
+      return (num / 1000 | 0) + "K";
     } else {
-        let millon = numero / 1000000;
-        if (millon % 1 >= 0.5) millon++; 
-        return (millon | 0) + "M"; 
+      return (num / 1000000 | 0) + "M";
     }
-}
-
+  }
 // código de prueba
 console.log(likes(983)) // "983"
 console.log(likes(1900)) // "1K"
